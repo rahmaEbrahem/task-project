179dfd8e8f63c54ec5ca77b8c4e3013c1aa6889f
rahmaEbrahim <rahmaebrahem149@gmail.com>
2026-05-28 17:44:35 +0300

    make search field

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_project11/widgets/customer_row.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,12 +8,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset("assets/images/Group.png"),
-              Row(children: [Icon(Icons.location_on), Text("Dhaka, Banassre")]),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 15),
+                Image.asset("assets/images/Group.png", width: 25, height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.location_on), Text("Dhaka, Banassre")],
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Search Store",
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight(600),
+                      color: Color(0xff7C7C7C),
+                    ),
+                    border: OutlineInputBorder(),
+                    fillColor: Color(0xffF2F3F2),
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                CustomerRow(title: "Exclusive Offer"),
+              ],
+            ),
           ),
         ),
       ),
